1393f92a00e0a1b88e6eead9c7b614a195bb78c2
Alexander1234paul <paquinatoau@utn.edu.ec>
2023-06-09 09:25:11 -0500

    End of table crud roles

--- controllers/administration/index.js
+++ controllers/administration/index.js
@@ -1,10 +1,33 @@
 const { db } = require("../../Conexiones/slq");
 
 /**
+ * GETS
+ */
+
+const getAllRoles = async (req, res) => {
+  try {
+    db.query(
+      "SELECT * FROM tbl_roles",
+      (error, results) => {
+        if (error) {
+          res.status(200).json({ message: error });
+        } else {
+          res.status(200).json(results.rows);
+        }
+      }
+    );
+  } catch (error) {
+    res.status(500).json({ message: "Error en el servidor" });
+  }
+};
+
+
+
+/**
  * POSTS
  */
 
-const addTest = async (req, res) => {
+const postTest = async (req, res) => {
   try {
     const {
       ID_PERSONA,
@@ -70,98 +93,112 @@ const addTest = async (req, res) => {
   }
 };
 
-const addPerson = async (req, res) => {
+const postCompany = async (req, res) => {
   try {
-    const { CI_RUC, NOMBRES, APELLIDOS, TELEFONO, EMAIL, DIRECCION } = req.body;
+    const {
+      CI_RUC,
+      NOMBRES,
+      APELLIDOS,
+      TELEFONO,
+      EMAIL,
+      DIRECCION,
+      RUC,
+      RAZON,
+      SLOGAN,
+      LOGO,
+      NOMBREA,
+      DIRECCIONA,
+      TELEFONOA,
+    } = req.body;
+
+    await db.query("BEGIN");
+    const queryPerson = `INSERT INTO tbl_personas(ci_ruc, nombres, apellidos, telefono, email, direccion) 
+                          VALUES ($1, $2, $3, $4, $5, $6) RETURNING id_persona`;
     const person = [CI_RUC, NOMBRES, APELLIDOS, TELEFONO, EMAIL, DIRECCION];
+    const resultPerson = await db.query(queryPerson, person);
+    const ID_PERSONA = resultPerson.rows[0].id_persona;
 
-    db.query(
-      "INSERT INTO public.tbl_personas(ci_ruc, nombres, apellidos, telefono, email, direccion) VALUES ($1, $2, $3, $4, $5, $6) RETURNING *",
-      person,
-      (error, results) => {
-        if (error) {
-          res.status(200).json({ message: error });
-        } else {
-          res.status(200).json(results.rows);
-        }
-      }
-    );
-
-    res.json({ message: "Successful insertions" });
-  } catch (error) {
-    res.status(500).json({ message: "Error en el servidor" });
-  }
-};
-
-const addCompany = async (req, res) => {
-  try {
-    const { ID_PERSONA, RUC, RAZON, SLOGAN, LOGO } = req.body;
-    if (ID_PERSONA === undefined) {
-      res
-        .status(400)
-        .json({ message: "Bad resquest. Please fill all fields." });
-    }
+    const queryCompany = `INSERT INTO tbl_empresas(id_persona, ruc, razon, slogan, logo) 
+                          VALUES ($1, $2, $3, $4, $5) RETURNING id_empresa`;
     const company = [ID_PERSONA, RUC, RAZON, SLOGAN, LOGO];
+    const resultCompany = await db.query(queryCompany, company);
+    const ID_EMPRESA = resultCompany.rows[0].id_empresa;
 
-    db.query(
-      "INSERT INTO public.tbl_empresas(id_persona, ruc, razon, slogan, logo) VALUES ($1, $2, $3, $4, $5) RETURNING *",
-      company,
-      (error, results) => {
-        if (error) {
-          res.status(200).json({ message: error });
-        } else {
-          res.status(200).json(results.rows);
-        }
-      }
-    );
+    const queryStore = `INSERT INTO tbl_almacenes(id_empresa, nombre, direccion, telefono) VALUES ($1, $2, $3, $4)`;
+    const store = [ID_EMPRESA, NOMBREA, DIRECCIONA, TELEFONOA];
+    await db.query(queryStore, store);
+
+    await db.query("COMMIT");
+
+    res.json({ mensaje: "Registro exitoso" });
   } catch (error) {
-    res.status(500).json({ message: "Error en el servidor" });
+    await db.query("ROLLBACK"); // Revertir la transacción en caso de error
+
+    // Respuesta de error
+    res.status(500).json({ error: "Error en el registro: " + error.message });
   }
 };
 
-const addStore = async (req, res) => {
+const postRol = async (req, res) => {
   try {
-    const { ID_EMPRESA, NOMBRE, DIRECCION, TELEFONO } = req.body;
-    if (ID_EMPRESA === undefined) {
-      res
-        .status(400)
-        .json({ message: "Bad resquest. Please fill all fields." });
-    }
-    const store = [ID_EMPRESA, NOMBRE, DIRECCION, TELEFONO];
-
-    db.query(
-      "INSERT INTO public.tbl_almacenes(id_empresa, nombre, direccion, telefono) VALUES ($1, $2, $3, $4) RETURNING *",
-      store,
-      (error, results) => {
-        if (error) {
-          res.status(200).json({ message: error });
-        } else {
-          res.status(200).json(results.rows);
-        }
+    const { ROL } = req.body;
+    db.query("INSERT INTO tbl_roles(rol) VALUES($1)", [ROL], (error, results) => {
+      if (error) {
+        res.send(`{"status":"Error", "resp":${error}}`);
+      } else {
+        res.send(`{"status":"Ok", "resp":"Successful insertions"}`);
+        // res.json({ message: "Successful insertions" });
       }
+    }
     );
   } catch (error) {
+    console.error("Error en la inserción:", error);
     res.status(500).json({ message: "Error en el servidor" });
   }
-};
+}
 
 /**
- * GETS
+ * PUTS
  */
 
-const GETTest = async (req, res) => {
+const updateRol = async (req, res) => {
   try {
-    db.query("select * from tbl_tipo_productos", (error, results) => {
+    const { ID_ROL, ROL } = req.body;
+    db.query("UPDATE tbl_roles SET rol=$1 WHERE id_rol=$2", [ROL, ID_ROL], (error, results) => {
       if (error) {
-        res.status(200).json({ message: error });
+        res.send(`{"status":"Error", "resp":${error}}`);
       } else {
-        res.status(200).json(results.rows);
+        res.send(`{"status":"Ok", "resp":"Successful update"}`);
+        // res.json({ message: "Successful insertions" });
       }
-    });
+    }
+    );
   } catch (error) {
+    console.error("Error en la actualización:", error);
     res.status(500).json({ message: "Error en el servidor" });
   }
-};
+}
+/**
+ * DELETES
+ */
 
-const getAllAlquiler = (request, response) => {};
-module.exports = { GETTest, addPerson, addCompany, addTest };
+const deleteRolById = async (req, res) => {
+  try {
+    const { ID_ROL } = req.params;
+    db.query("DELETE FROM tbl_roles WHERE id_rol=$1", [ID_ROL],
+      (error, results) => {
+        if (error) {
+          console.error('Error en la eliminación', error);
+          res.status(500).json({ error: 'Error en la eliminación' });
+        } else {
+          res.status(200).json({ message: 'Registro eliminado correctamente' });
+        }
+      }
+    );
+  } catch (error) {
+    console.error("Error en la eliminación:", error);
+    res.status(500).json({ message: "Error en el servidor" });
+  }
+}
+
+module.exports = { getAllRoles, postCompany, postRol, updateRol, deleteRolById };
--- routes/administration/index.js
+++ routes/administration/index.js
@@ -1,7 +1,11 @@
 const Router = require("express");
-const { addCompany } = require("../../controllers/administration/index");
+const { postCompany, postRol, getAllRoles, updateRol, deleteRolById } = require("../../controllers/administration/index");
 
 const router = Router();
-router.post("/", addCompany);
+router.post("/administration", postCompany);
+router.post("/administration/rol", postRol);
+router.get("/administration/rol", getAllRoles);
+router.put("/administration/rol", updateRol);
+router.delete("/administration/rol/:ID_ROL", deleteRolById);
 
 module.exports = router;
